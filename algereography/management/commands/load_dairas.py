52c1d0e13070d9d49a0fe0312216e2852ce71ce6
poysa213 <y.hanaia@esi-sba.dz>
2023-02-24 01:43:01 +0100

    create commands to load wilayas and dairas

---
+++ algereography/management/commands/load_dairas.py
@@ -0,0 +1,18 @@
+from typing import Any, Optional
+import json
+from django.core.management.base import BaseCommand
+from algereography.models import Wilaya, Daira
+
+class Command(BaseCommand):
+    help = "Load algeria wilaya and daira data to the db"
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        with open('algereography/data/daira.json', 'r') as f:
+            data = json.load(f)
+            
+
+        for item in data:
+            wilaya = Wilaya.objects.get(pk=item['fields']['wilaya'])
+            daira = Daira(ar_name=item['fields']['ar_name'], name=item['fields']['name'],wilaya=wilaya)
+            daira.save()
+            
+        self.stdout.write(self.style.SUCCESS(('All Dairas successfully loaded')))
---
+++ algereography/management/commands/load_wilayas.py
@@ -0,0 +1,17 @@
+from typing import Any, Optional
+import json
+from django.core.management.base import BaseCommand
+from algereography.models import Wilaya
+
+class Command(BaseCommand):
+    help = "Load algeria wilaya and daira data to the db"
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        with open('algereography/data/wilaya.json', 'r') as f:
+            data = json.load(f)
+            
+
+        for item in data:
+            wilaya = Wilaya(ar_name=item['fields']['ar_name'], name=item['fields']['name'],)
+            wilaya.save()
+            
+        self.stdout.write(self.style.SUCCESS(('All wilayas successfully loaded')))
